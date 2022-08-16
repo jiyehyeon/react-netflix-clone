52a6ba584f8f4d068f9cd8306a4872cfb6c2324c
jiyehyeon <hyeojniyeah@gmail.com>
2022-08-17 05:44:53 +0900

    axios 인스턴스 생성

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themovieddb.org/3",
+  params: {
+    api_key: "3e99b7d0769fe61a752c48436558e183",
+    language: "ko-KR",
+  },
+});
+
+export default instance;
