bb649e368c5186ae531f8e8feee86af237701495
jiyehyeon <hyeojniyeah@gmail.com>
2022-08-18 05:18:19 +0900

    Row 슬라이드 구현

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,6 +4,7 @@ import "./Row.css";
 
 export default function Row({ title, fetchUrl, isLargeRow, id }) {
   const [movies, setMovies] = useState([]);
+  const [slideIndex, setSlideIndex] = useState(0);
 
   useEffect(() => {
     fetchMovieData();
@@ -15,12 +16,36 @@ export default function Row({ title, fetchUrl, isLargeRow, id }) {
     console.log(request.data.results);
   };
 
+  const slideRight = (id) => {
+    const elem = document.getElementById(id);
+    const contentWidth = elem.querySelector(".row__poster").offsetWidth;
+    let newSlideIndex = slideIndex + 6;
+    if (newSlideIndex > movies.length - 6) {
+      newSlideIndex = movies.length - 6;
+    }
+    elem.style.transform = `translateX(-${newSlideIndex * contentWidth}px)`;
+    setSlideIndex(newSlideIndex);
+  };
+
+  const slideLeft = (id) => {
+    const elem = document.getElementById(id);
+    const contentWidth = elem.querySelector(".row__poster").offsetWidth;
+    let newSlideIndex = slideIndex - 6;
+    if (newSlideIndex < 0) {
+      newSlideIndex = 0;
+    }
+    elem.style.transform = `translateX(-${newSlideIndex * contentWidth}px)`;
+    setSlideIndex(newSlideIndex);
+  };
+
   return (
     <section className="row">
       <h2 className="row-header-title">{title}</h2>
       <div className="slider">
         <div className="slider__arrow left">
-          <span className="arrow">{"<"}</span>
+          <span className="arrow" onClick={() => slideLeft(id)}>
+            {"<"}
+          </span>
         </div>
         <div id={id} className="slider__items">
           {movies.map((movie) => {
@@ -37,7 +62,14 @@ export default function Row({ title, fetchUrl, isLargeRow, id }) {
           })}
         </div>
         <div className="slider__arrow right">
-          <span className="arrow">{">"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              slideRight(id);
+            }}
+          >
+            {">"}
+          </span>
         </div>
       </div>
     </section>
