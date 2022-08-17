92febe22c95b7172ef1ebf1d078be1f183d54876
jiyehyeon <hyeojniyeah@gmail.com>
2022-08-17 23:38:50 +0900

    메인 배너에 노출될 영화 정보 호출

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.themovieddb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "3e99b7d0769fe61a752c48436558e183",
     language: "ko-KR",
---
+++ src/components/Banner.js
@@ -0,0 +1,26 @@
+import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
+import requests from "../api/requests";
+
+export default function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    // 현재 상여중인 영화 정보 가져오기
+    const request = await axios.get(requests.fetchNowPlaying);
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+    setMovie(movieDetail);
+  };
+
+  return <div></div>;
+}
