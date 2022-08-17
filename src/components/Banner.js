43040721c7e699a96f445e0e0480fe8353eb0178
jiyehyeon <hyeojniyeah@gmail.com>
2022-08-18 00:18:42 +0900

    배너 컴포넌트 구현

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
 import requests from "../api/requests";
+import "./Banner.css";
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
@@ -10,7 +11,7 @@ export default function Banner() {
   }, []);
 
   const fetchData = async () => {
-    // 현재 상여중인 영화 정보 가져오기
+    // 현재 상영중인 영화 정보 가져오기
     const request = await axios.get(requests.fetchNowPlaying);
     const movieId =
       request.data.results[
@@ -20,7 +21,32 @@ export default function Banner() {
       params: { append_to_response: "videos" },
     });
     setMovie(movieDetail);
+    console.log(movieDetail);
   };
 
-  return <div></div>;
+  const truncate = (str, n) => {
+    if (str) return str.length > n ? str.substr(0, n - 1) + "..." : str;
+    return "";
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie.title || movie.original_title}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">재생하기</button>
+          <button className="banner__button info">더 알아보기</button>
+        </div>
+        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 }
